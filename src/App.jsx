fa5d878647e7498ffc96c273bbc564a0f1c054e9
zidane-arkan <zidark897@Gmail.com>
2023-03-25 07:25:09 +0700

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Login from './components/Login';
+
+function App() {
+
+  return (
+    <div className="w-full">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ui/Input.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Input = React.forwardRef((props, ref) => {
+    return (
+        <div>
+
+        </div>
+    );
+});
+
+export default Input;
