82ce74ba1ca485f8384a567de710c6e8224091fb
zidane-arkan <zidark897@Gmail.com>
2023-03-25 10:40:46 +0700

    Update login & Input

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,14 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import Input from './ui/Input.jsx';
 const Login = () => {
+    const [email, setEmail] = useState();
+    const emailValue = useRef();
+
+    const submitValue = (e) => {
+        e.preventDefault();
+        console.log(email);
+        console.log(emailValue.current.value);
+    };
     return (
         <section className="bg-white dark:bg-gray-900">
             <div className="flex justify-center h-screen">
@@ -24,41 +33,9 @@ const Login = () => {
                             <p className="mt-3 text-gray-500 dark:text-gray-300">Sign in to access your account</p>
                         </div>
                         <div className="mt-8">
-                            <form>
-                                <div>
-                                    <label htmlFor="email" className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Email Address</label>
-                                    <input
-                                        type="email"
-                                        name="email"
-                                        id="email"
-                                        placeholder="example@example.com"
-                                        className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-                                    />
-                                </div>
-                                <div className="mt-6">
-                                    <div className="flex justify-between mb-2">
-                                        <label
-                                            htmlFor="password"
-                                            className="text-sm text-gray-600 dark:text-gray-200"
-                                        >
-                                            Password
-                                        </label>
-                                        <a
-                                            href="#"
-                                            className="text-sm text-gray-400 focus:text-blue-500 hover:text-blue-500 hover:underline"
-                                        >
-                                            Forgot password?
-                                        </a>
-                                    </div>
-                                    <input
-                                        type="password"
-                                        name="password"
-                                        id="password"
-                                        placeholder="Your Password"
-                                        className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-                                    />
-                                </div>
-
+                            <form onSubmit={submitValue}>
+                                <Input ref={emailValue} statusInput={"email"} />
+                                <Input classValue="mt-6" statusInput={"password"} />
                                 <div className="mt-6">
                                     <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50">
                                         Sign in
--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
-const Input = (props) => {
+const Input = React.forwardRef((props,ref) => {
     return (
-        <div>
-
+        <div className={props.classValue}>
+            <label htmlFor={props.statusInput} className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Email Address</label>
+            <input
+                ref={ref}
+                type={props.statusInput}
+                name={props.statusInput}
+                id={props.statusInput}
+                placeholder="example@example.com"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
         </div>
     );
-};
+});
 
 export default Input;
